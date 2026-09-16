b563279d94942b38e53cf47ae9c1a420ce643498
Lê Thành Nguyễn <lethanhnguyen200617@gmail.com>
2026-09-16 18:18:04 +0700

    Update RUN.bat
    update Run.bat

--- RUN.bat
+++ RUN.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0run.ps1"
-if errorlevel 1 pause
+start "" powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0run.ps1"
+exit /b
